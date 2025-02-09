e6a524620488ccc4d334b4db0be28646894cab0b
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2025-02-09 13:51:11 +0530

    Update userRouter.js
    change the code

--- backend/Routes/userRouter.js
+++ backend/Routes/userRouter.js
@@ -1,32 +1,43 @@
 
+// const express = require("express");
+// const { body } = require("express-validator");
+// const { signup, login, verifyUser } = require("../controllers/userController");
+// const authenticateToken = require("../middleware/jwtMiddleware"); // Create middleware separately
+
+// const router = express.Router();
+
+// // Signup Route
+// router.post(
+//   "/signup",
+//   [
+//     body("email").isEmail().withMessage("Invalid email format"),
+//     body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+//   ],
+//   signup
+// );
+
+// // Login Route
+// router.post(
+//   "/login",
+//   [
+//     body("email").isEmail().withMessage("Invalid email format"),
+//     body("password").notEmpty().withMessage("Password is required"),
+//   ],
+//   login
+// );
+
+// // Verify Token Route (Protected)
+// router.get("/verify", authenticateToken, verifyUser);
+
+// module.exports = router;
+// ---------------------------------------------------------
 const express = require("express");
-const { body } = require("express-validator");
-const { signup, login, verifyUser } = require("../controllers/userController");
-const authenticateToken = require("../middleware/jwtMiddleware"); // Create middleware separately
+const authenticateToken = require("../middleware/jwtMiddleware");
 
 const router = express.Router();
 
-// Signup Route
-router.post(
-  "/signup",
-  [
-    body("email").isEmail().withMessage("Invalid email format"),
-    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
-  ],
-  signup
-);
-
-// Login Route
-router.post(
-  "/login",
-  [
-    body("email").isEmail().withMessage("Invalid email format"),
-    body("password").notEmpty().withMessage("Password is required"),
-  ],
-  login
-);
-
-// Verify Token Route (Protected)
-router.get("/verify", authenticateToken, verifyUser);
+router.get("/verify", authenticateToken, (req, res) => {
+  res.json({ message: "Token is valid", user: req.user });
+});
 
 module.exports = router;
