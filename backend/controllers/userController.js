d56eccbf0db8fc18bf8113d79da4e16541b1c4fa
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2025-02-09 11:05:40 +0530

    Update userController.js
    decrypt comments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 const User = require("../models/userModel");
